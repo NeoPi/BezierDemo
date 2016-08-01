3980e8a64f9f15aa8d87a8b992644e4ccfcb77a2
biyiguang <biyigunag@xiaomi.com>
2016-08-01 17:54:51 +0800

    提交拖动贝塞尔的动画

--- app/src/main/java/com/neopi/bezier/widget/DragBezier.java
+++ app/src/main/java/com/neopi/bezier/widget/DragBezier.java
@@ -3,6 +3,7 @@ package com.neopi.bezier.widget;
 import android.animation.PropertyValuesHolder;
 import android.animation.ValueAnimator;
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
@@ -10,9 +11,9 @@ import android.graphics.Path;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
-import android.view.animation.AccelerateDecelerateInterpolator;
+import android.view.animation.AccelerateInterpolator;
 import android.view.animation.BounceInterpolator;
-import android.view.animation.OvershootInterpolator;
+import com.neopi.bezier.R;
 
 /**
  * Created by neopi on 16-7-29.
@@ -22,30 +23,60 @@ public class DragBezier extends View {
   private static final String TAG = "RectangleBezier";
 
   private Paint mPaint ;  // 圆形画笔
-  private Path mPath ;
+  private Paint mTextPaint ;
+  private Path mPath;
 
-  private int mPointOneX ; // 第一个圆的圆心坐标
-  private int mPointOneY ;
+  private int mPaintcolor ;
+  private int mTextPaintColor ;
+  private String text;
+  private int mTextSize;
 
-  private int mPointTwoX ; // 第二个圆的圆心坐标
-  private int mPointTwoY ;
+  private int mPointOneX; // 第一个圆的圆心坐标
+  private int mPointOneY;
 
-
-  private int color = Color.RED;
+  private int mPointTwoX; // 第二个圆的圆心坐标
+  private int mPointTwoY;
 
   private int RADIUS = 50;
+  private int breakLen = 300; // 拉断距离
 
   public DragBezier(Context context) {
-    this(context,null);
+    this(context, null);
   }
 
   public DragBezier(Context context, AttributeSet attrs) {
-    this(context, attrs,0);
+    this(context, attrs, 0);
   }
 
   public DragBezier(Context context, AttributeSet attrs, int defStyleAttr) {
     super(context, attrs, defStyleAttr);
+
+    initValues(context, attrs, defStyleAttr);
     initData();
+  }
+
+  private void initValues(Context context, AttributeSet attrs, int defStyleAttr) {
+    TypedArray mTypeValue = context.obtainStyledAttributes(attrs, R.styleable.DragBezier,defStyleAttr,0);
+
+    int count = mTypeValue.getIndexCount();
+    for (int i = 0; i < count ; i++){
+      int attr = mTypeValue.getIndex(i);
+      switch (attr){
+        case R.styleable.DragBezier_dragColor:
+          mPaintcolor = mTypeValue.getColor(i,Color.RED);
+          break;
+        case R.styleable.DragBezier_dragText:
+          text = mTypeValue.getString(i);
+          break;
+        case R.styleable.DragBezier_dragTextColor:
+          mTextPaintColor = mTypeValue.getColor(i,Color.BLACK);
+          break;
+
+        case R.styleable.DragBezier_dragTextSize:
+          mTextSize = mTypeValue.getDimensionPixelSize(i,18);
+      }
+    }
+    mTypeValue.recycle();
   }
 
   private void initData() {
@@ -53,21 +84,26 @@ public class DragBezier extends View {
 
     mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
     mPaint.setStyle(Paint.Style.FILL);
+    mPaint.setColor(mPaintcolor);
 
-    mPointOneX = 300 ;
-    mPointOneY = 300 ;
+    mTextPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+    mTextPaint.setStyle(Paint.Style.FILL);
+    mTextPaint.setColor(mTextPaintColor);
+    mTextPaint.setTextSize(mTextSize);
+
+    mPointOneX = 300;
+    mPointOneY = 300;
 
     mPointTwoX = mPointOneX + 300;
     mPointTwoY = mPointOneY + 300;
-
   }
 
-  public void setColor(int color){
-    this.color = color;
+  public void setColor(int color) {
+    this.mPaintcolor = color;
   }
 
   @Override protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-    super.onMeasure(widthMeasureSpec,heightMeasureSpec);
+    super.onMeasure(widthMeasureSpec, heightMeasureSpec);
     //int width = MeasureSpec.getSize(widthMeasureSpec);
     //int height = MeasureSpec.getSize(heightMeasureSpec);
     //int wid = Math.min(width,height);
@@ -79,72 +115,47 @@ public class DragBezier extends View {
   @Override protected void onDraw(Canvas canvas) {
     super.onDraw(canvas);
     mPath.reset();
-    mPaint.setColor(color);
 
-    int mControlPointX = (mPointTwoX + mPointOneX) / 2 ;
-    int mControlPointY = (mPointTwoY + mPointOneY) / 2 ;
+    int mControlPointX = (mPointTwoX + mPointOneX) / 2;
+    int mControlPointY = (mPointTwoY + mPointOneY) / 2;
 
-    int dx = mControlPointX - mPointOneX ;
-    int dy = mControlPointY - mPointOneY ;
+    int dx = mControlPointX - mPointOneX;
+    int dy = mControlPointY - mPointOneY;
 
     /* 控制点到起始点的距离 */
     double distance = Math.sqrt((dx * dx) + (dy * dy));
     double acos = Math.acos(RADIUS / distance);
 
-    //if (mPointTwoY >= mPointTwoX){
-      double acos1 = Math.acos( dx / distance );
-      int x1 = mPointOneX + (int) (RADIUS * Math.cos(acos - acos1));
-      int y1 = mPointOneY - (int) (RADIUS * Math.sin(acos - acos1));
+    double acos1 = Math.acos(dx / distance);
+    int x1 = mPointOneX + (int) ((RADIUS - 10) * Math.cos(acos - acos1));
+    int y1 = mPointOneY - (int) ((RADIUS - 10) * Math.sin(acos - acos1));
 
-      double acos2 = Math.asin( dx / distance );
-      int x2 = mPointOneX - (int)(RADIUS * Math.sin(acos - acos2));
-      int y2 = mPointOneY + (int)(RADIUS * Math.cos(acos - acos2));
+    double acos2 = Math.asin(dx / distance);
+    int x2 = mPointOneX - (int) ((RADIUS - 10) * Math.sin(acos - acos2));
+    int y2 = mPointOneY + (int) ((RADIUS - 10) * Math.cos(acos - acos2));
 
-      int x3 = mPointTwoX + (int) (RADIUS * Math.sin(acos - acos2));
-      int y3 = mPointTwoY - (int) (RADIUS * Math.cos(acos - acos2));
+    int x3 = mPointTwoX + (int) (RADIUS * Math.sin(acos - acos2));
+    int y3 = mPointTwoY - (int) (RADIUS * Math.cos(acos - acos2));
 
-      int x4 = mPointTwoX - (int) (RADIUS * Math.cos(acos - acos1));
-      int y4 = mPointTwoY + (int) (RADIUS * Math.sin(acos - acos1));
+    int x4 = mPointTwoX - (int) (RADIUS * Math.cos(acos - acos1));
+    int y4 = mPointTwoY + (int) (RADIUS * Math.sin(acos - acos1));
     /* (x1,y1),(x2,y2),(x3,y3) ,(x4,y4) 这四个点分别对应第一个圆与第二个圆上的四个切点*/
 
-      mPath.moveTo(x1,y1);
-      mPath.quadTo(mControlPointX,mControlPointY,x3,y3);
-      mPath.lineTo(x4,y4);
-      mPath.quadTo(mControlPointX,mControlPointY,x2,y2);
-    //} else {
-    //  double acos1 = Math.acos( dx / distance );
-    //  int x1 = mPointOneX - (int) (RADIUS * Math.cos(acos - acos1));
-    //  int y1 = mPointOneY + (int) (RADIUS * Math.sin(acos - acos1));
-    //
-    //  double acos2 = Math.asin( dx / distance );
-    //  int x2 = mPointOneX + (int)(RADIUS * Math.sin(acos - acos2));
-    //  int y2 = mPointOneY - (int)(RADIUS * Math.cos(acos - acos2));
-    //
-    //  int x3 = mPointTwoX - (int) (RADIUS * Math.sin(acos - acos2));
-    //  int y3 = mPointTwoY + (int) (RADIUS * Math.cos(acos - acos2));
-    //
-    //  int x4 = mPointTwoX + (int) (RADIUS * Math.cos(acos - acos1));
-    //  int y4 = mPointTwoY - (int) (RADIUS * Math.sin(acos - acos1));
-    ///* (x1,y1),(x2,y2),(x3,y3) ,(x4,y4) 这四个点分别对应第一个圆与第二个圆上的四个切点*/
-    //
-    //  mPath.moveTo(x1,y1);
-    //  mPath.quadTo(mControlPointX,mControlPointY,x3,y3);
-    //  mPath.lineTo(x4,y4);
-    //  mPath.quadTo(mControlPointX,mControlPointY,x2,y2);
-    //}
 
+    mPath.moveTo(x1, y1);
+    mPath.quadTo(mControlPointX, mControlPointY, x3, y3);
+    mPath.lineTo(x4, y4);
+    mPath.quadTo(mControlPointX, mControlPointY, x2, y2);
+    mPath.lineTo(x1, y1);
 
-
-    canvas.drawCircle(mPointOneX,mPointOneY,RADIUS,mPaint);
-    canvas.drawCircle(mPointTwoX,mPointTwoY,RADIUS,mPaint);
-
-
-    canvas.drawPath(mPath,mPaint);
-
+    canvas.drawCircle(mPointOneX, mPointOneY, RADIUS - 10, mPaint);
+    canvas.drawCircle(mPointTwoX, mPointTwoY, RADIUS, mPaint);
+    canvas.drawPath(mPath, mPaint);
+    canvas.drawText(text,(mPointTwoX-mTextPaint.measureText(text) / 2),(mPointTwoY+mTextPaint.measureText(text,0,1) / 2),mTextPaint);
   }
 
   @Override public boolean onTouchEvent(MotionEvent event) {
-    switch (event.getAction()){
+    switch (event.getAction()) {
       case MotionEvent.ACTION_DOWN:
         mPointTwoX = (int) event.getX();
         mPointTwoY = (int) event.getY();
@@ -153,19 +164,50 @@ public class DragBezier extends View {
       case MotionEvent.ACTION_MOVE:
         mPointTwoX = (int) event.getX();
         mPointTwoY = (int) event.getY();
-        invalidate();
+        int lenght =
+            (int) Math.sqrt((mPointTwoX - mPointOneX)*(mPointTwoX - mPointOneX) + (mPointTwoY - mPointOneY)*(mPointTwoY - mPointOneY));
+        if (lenght > breakLen){
+          hindPointOne();
+        } else {
+          invalidate();
+        }
         break;
       case MotionEvent.ACTION_UP:
-        reset();
+        mPointTwoX = (int) event.getX();
+        mPointTwoY = (int) event.getY();
+        int distance =
+            (int) Math.sqrt((mPointTwoX - mPointOneX)*(mPointTwoX - mPointOneX) + (mPointTwoY - mPointOneY)*(mPointTwoY - mPointOneY));
+        if (distance <= breakLen) {
+          reset();
+        }
         break;
     }
     return true;
   }
 
+  /**
+   *
+   */
+  private void hindPointOne() {
+    PropertyValuesHolder mHolderX = PropertyValuesHolder.ofInt("X", mPointOneX, mPointTwoX);
+    PropertyValuesHolder mHolderY = PropertyValuesHolder.ofInt("Y", mPointOneY, mPointTwoY);
+    ValueAnimator animator = ValueAnimator.ofPropertyValuesHolder(mHolderX, mHolderY);
+    animator.setDuration(1);
+    //animator.setInterpolator(new AccelerateInterpolator());
+    animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+      @Override public void onAnimationUpdate(ValueAnimator animation) {
+        mPointOneX = (int) animation.getAnimatedValue("X");
+        mPointOneY = (int) animation.getAnimatedValue("Y");
+        invalidate();
+      }
+    });
+    animator.start();
+  }
+
   private void reset() {
-    PropertyValuesHolder mHolderX = PropertyValuesHolder.ofInt("X",mPointTwoX,mPointOneX);
-    PropertyValuesHolder mHolderY = PropertyValuesHolder.ofInt("Y",mPointTwoY,mPointOneY);
-    ValueAnimator animator = ValueAnimator.ofPropertyValuesHolder(mHolderX,mHolderY);
+    PropertyValuesHolder mHolderX = PropertyValuesHolder.ofInt("X", mPointTwoX, mPointOneX);
+    PropertyValuesHolder mHolderY = PropertyValuesHolder.ofInt("Y", mPointTwoY, mPointOneY);
+    ValueAnimator animator = ValueAnimator.ofPropertyValuesHolder(mHolderX, mHolderY);
     animator.setInterpolator(new BounceInterpolator());
     animator.setDuration(500);
     animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
